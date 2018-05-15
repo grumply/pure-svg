cad52588584be2c86ed2bcad878ffcbeba874fa5
S. M. Hickman <sean@grump.ly>
2018-05-14 19:09:37 -0500

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,9 @@
+{ mkDerivation, base, pure-core, pure-txt, stdenv }:
+mkDerivation {
+  pname = "pure-svg";
+  version = "0.7.0.0";
+  src = ./.;
+  libraryHaskellDepends = [ base pure-core pure-txt ];
+  homepage = "github.com/grumply/pure-svg";
+  license = stdenv.lib.licenses.bsd3;
+}
