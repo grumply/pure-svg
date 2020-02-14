78ca9432425f12974c075ce2c3251383c268ae03
Sean Hickman <sean@grumply.com>
2020-02-13 23:36:29 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, pure-core, pure-txt, stdenv }:
 mkDerivation {
   pname = "pure-svg";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base pure-core pure-txt ];
   homepage = "github.com/grumply/pure-svg";
